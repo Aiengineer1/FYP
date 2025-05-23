d4e0be7791ae6d9173c481fa90878ddce1923e30
Anas Ilyas <024276@MSP-07.IPC.EDU>
2025-05-23 14:08:07 +0500

    Update backend with new virtual environment and alembic configuration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,7 @@
+from ..database import Base
 from .user import User
 from .mall import Mall
 from .camera import Camera
 from .customer import Customer
+
+__all__ = ['Base', 'User', 'Mall', 'Camera', 'Customer']
